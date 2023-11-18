cc242906e32660f1acc89089ce3fb974b3418252
pavel.lukes <pavel.lukes@direct.cz>
2023-11-18 11:58:54 +0100

    refaktor podle poslednich uprav

--- pages/gpt4.tsx
+++ pages/gpt4.tsx
@@ -1,13 +1,9 @@
 // app/page.tsx
 "use client";
 
-import React, {useState, useEffect, useRef} from "react";
-import ChatManager from '../app/services/ChatManager';
-import LinkBar from '../app/components/LinkBar';
-import MessageList from '../app/components/MessageList';
-import WelcomeForm from '../app/components/WelcomeForm';
-import InputForm from '../app/components/InputForm';
-import {useChatState} from '@/app/hooks/useChatState';
+
+import {LinkBar, MessageList, WelcomeForm, InputForm} from '../app/components';
+import {useChatState, useChatManager, useStartAssistant} from '@/app/hooks';
 
 import "../app/globals.css";
 
@@ -28,61 +24,19 @@ export default function Chat() {
         inputRef,
         formRef,
         initialThreadMessage,
-        setInitialThreadMessage
-
+        setInitialThreadMessage,
+        setChatStarted,
+        chatStarted: chatHasStarted,
+        chatManager, setChatManager,
+        assistantId,
+        isMessageLoading, setIsMessageLoading,
+        progress, setProgress,
+        isLoadingFirstMessage,
+        setIsLoadingFirstMessage
     } = useChatState();
 
-    // Initialize ChatManager only once using useEffect
-    const [chatManager, setChatManager] = useState<ChatManager | null>(null);
-
-    // Add a state to track loading status of message sending
-    const [isMessageLoading, setIsMessageLoading] = useState(false);
-
-    useEffect(() => {
-
-
-        startChatAssistant().then(() => {
-            console.log('assistant started');
-        });
-        // Update isMessageLoading based on the chatManager's isLoading state
-
-
-    }, [setChatMessages, setStatusMessage]);
-
-    // Update chat state and handle assistant response reception
-
-
-    const startChatAssistant = async () => {
-        console.log('start chat assistant')
-        setIsButtonDisabled(true);
-        setStartLoading(true);
-
-        const chatManagerInstance = ChatManager.getInstance(setChatMessages, setStatusMessage);
-        setChatManager(chatManagerInstance);
-
-        if (chatManagerInstance) {
-            setIsMessageLoading(chatManagerInstance.getChatState().isLoading);
-            try {
-                let searchParams = new URLSearchParams(window.location.search);
-                let assistantIdParam = searchParams.get("assistantId");
-
-                if (!assistantIdParam) {
-                    assistantIdParam = assId;
-                }
-                const assistantId = assistantIdParam;
-                await chatManagerInstance.startAssistantById(assistantId, 'Introduce yourself');
-                console.log('Assistant started:', chatManagerInstance.getChatState());
-            } catch (error) {
-                console.error('Error starting assistant:', error);
-                if (error instanceof Error) setStatusMessage(`Error: ${error.message}`);
-            } finally {
-                setIsButtonDisabled(false);
-                setStartLoading(false);
-            }
-
-        }
-
-    };
+    useChatManager(setChatMessages, setStatusMessage, setChatManager, setIsMessageLoading, setProgress, setIsLoadingFirstMessage);
+    useStartAssistant(assId, chatManager, 'Introduce yourself');
 
     const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -111,9 +65,11 @@ export default function Chat() {
 
     return (
         <main className="flex flex-col items-center justify-between pb-40 bg-space-grey-light">
-            <img src="https://www.direct-technologies.cz/static/header/direct-technologies-black.svg" alt="logo" style={{width: "100px", marginTop:"10px", marginBottom: "20px"}}/>
-            {chatMessages.length > 0 ? (
-                <MessageList chatMessages={chatMessages}/>
+            <img src="https://www.direct-technologies.cz/static/header/direct-technologies-black.svg" alt="logo"
+                 style={{width: "100px", marginTop: "10px", marginBottom: "20px"}}/>
+            {chatHasStarted || assistantId || isLoadingFirstMessage ? (
+                <MessageList chatMessages={chatMessages} statusMessage={statusMessage} isSending={isSending}
+                             progress={progress} isFirstMessage={isLoadingFirstMessage}/>
             ) : (
                 <span>navazuji komunikaci s GPT ...</span>
             )}
