c2b09fd8393baf8b840141166798ea852fed5842
Radim Kurka <radim.kurka@direct.cz>
2023-12-08 14:18:50 +0100

    fix typing

--- app/components/MessageList.tsx
+++ app/components/MessageList.tsx
@@ -4,36 +4,52 @@ import { Bot, User } from "lucide-react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { ImageIcon, DocumentIcon } from '@/app/icons';
+import { FC } from "react";
 
+export interface MessageListProps {
+  chatMessages?: Message[];
+  statusMessage: string;
+  isSending?: boolean;
+  progress?: any;
+  isFirstMessage?: boolean;
 
-export interface Message {
-  content?: string;
-  role?: string;
-  fileDetails?: Array<any>;
-  statusMessage?: string;
-  isLoading?: boolean;
 }
 
 
+export interface MessageProps {
+  progress?: any;
+    fileDetails?: Array<any>;
+  isFirstMessage?: boolean;
+  message?: Message;
+}
+
+export interface Message {
+  content?: string;
+  fileDetails?: Array<any>;
+  isLoading?: boolean;
+  role?: string;
+  statusMessage?: string;
+}
+
 // Message component to display individual messages
-const Message = ({ message, progress, isFirstMessage, fileDetails }) => {
+const Message: FC<MessageProps> = ({ message, progress, isFirstMessage, fileDetails }) => {
   return (
     <div
       className={clsx(
         "flex w-full items-center justify-center border-b border-gray-200 py-8",
-        message.role === "user" ? "bg-white" : "bg-gray-100"
+        message?.role === "user" ? "bg-white" : "bg-gray-100"
       )}
     >
       <div className="flex w-full max-w-screen-md items-start space-x-4 px-5 sm:px-0">
         <div
           className={clsx(
             "p-1.5 text-white",
-            message.role === "assistant" ? "bg-green-500" : "bg-black"
+            message?.role === "assistant" ? "bg-green-500" : "bg-black"
           )}
         >
-          {message.role === "user" ? <User width={20} /> : <Bot width={20} />}
+          {message?.role === "user" ? <User width={20} /> : <Bot width={20} />}
         </div>
-        {message.role === "assistant" && message.isLoading ? (
+        {message?.role === "assistant" && message.isLoading ? (
           <>
             <div className="w-full h-4 bg-gray-200 rounded-full overflow-hidden">
               <div className={clsx("h-full bg-green-500", isFirstMessage ? "animate-spin-slow" : "")} style={{ width: `${progress}%` }}></div>
@@ -53,7 +69,7 @@ const Message = ({ message, progress, isFirstMessage, fileDetails }) => {
                 ),
               }}
             >
-              {message.content}
+              {message?.content}
             </ReactMarkdown>
             <div className="grid grid-cols-4 gap-2 mt-2">
               {fileDetails && fileDetails.map((file) => (
@@ -71,8 +87,8 @@ const Message = ({ message, progress, isFirstMessage, fileDetails }) => {
 };
 
 // MessageList component to display a list of messages
-const MessageList = ({ chatMessages, statusMessage, isSending, progress, isFirstMessage, fileDetails }) => {
-  let messages = [...chatMessages];
+const MessageList: FC<MessageListProps> = ({ chatMessages, statusMessage, isSending, progress, isFirstMessage}) => {
+  let messages = [...chatMessages!];
 
   // Add a loading message when the site loads and isFirstMessage is true
   if (isFirstMessage && messages.length === 0) {
