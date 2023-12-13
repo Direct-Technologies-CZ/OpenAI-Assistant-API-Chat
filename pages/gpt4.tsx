0545fa876bf86e486efeb2c6da7cd5be58c8b74a
Radim Kurka <radim.kurka@direct.cz>
2023-12-13 13:16:06 +0100

    add route redirect to homepage from old URL

---
+++ pages/gpt4.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+
+const NotFoundPage: React.FC = () => {
+  useEffect(() => {
+    // Redirect to home page after a short delay
+    const timer = setTimeout(() => {
+      window.location.href = '/';
+    }, 1000); // Redirect after 3 seconds
+
+    // Clean up the timer when the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>You will be redirected to the homepage in 1 second...</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
